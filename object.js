c1d2a57223679bfe6d56cd507daa7ca60731e2e4
Your Name <you@example.com>
2025-10-20 09:00:07 +0300

    objects uzdevums ir pabeigts

---
+++ object.js
@@ -0,0 +1,13 @@
+let car = {
+  marku: "BMW",
+  modelis: "s25",
+  gads: 2014,
+};
+
+console.log(car.modelis);
+
+if (car.gads < 2015) {
+  console.log("Mašīna ir vecāka par 10 gadiem");
+} else {
+  console.log("Mašīna nav vecaka par 10 gadiem");
+}
